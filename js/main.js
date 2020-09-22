96b10a28563555b225f6aa8065c53a7d8816353a
Didi <didi@didi.com>
2020-09-22 13:56:04 -0300

    1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Var
+var nome = "Rogério Skylab";
+var idade = 21;
+var idade2 = 22;
+var frase = "Flamengo é o melhor time da Europa";
+
+alert("Eae " +nome +" de " +idade +", bem-vindo viadão!")
+alert(frase.replace("Flamengo", "Vasco"));
+
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Flamengo", "Vasco"));
+console.log(frase.toUpperCase());
